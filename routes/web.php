356a937e74eab424b0ac047d1899bf7e6ecf00de
ABDALRHMAN#PRO <abdalrhman>
2024-02-12 14:48:17 +0200

    my changes

--- routes/web.php
+++ routes/web.php
@@ -129,3 +129,6 @@ Auth::routes(['verify' => true]);
 
 Route::get('/home', 'HomeController@index')->name('home')->middleware('verified');
 
+Route::get('/redirect/{service}','SocialController@redirect');
+
+Route::get('/callback/{service}','SocialController@callback');
